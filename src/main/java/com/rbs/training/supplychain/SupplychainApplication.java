778ce0e1a1d53982636e62dc4ff52041cba042a8
johnson-jose <johnsonchoonattu@gmail.com>
2017-08-17 09:54:07 -0700

    Sample REST Service

--- src/main/java/com/rbs/training/supplychain/SupplychainApplication.java
+++ src/main/java/com/rbs/training/supplychain/SupplychainApplication.java
@@ -2,9 +2,12 @@ package com.rbs.training.supplychain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.rbs.training.supplychain.controller")
 public class SupplychainApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupplychainApplication.class, args);
---
+++ src/main/java/com/rbs/training/supplychain/controller/InvoiceController.java
@@ -0,0 +1,15 @@
+package com.rbs.training.supplychain.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/supplychain")
+public class InvoiceController {
+	
+    @RequestMapping("/greeting")
+    public String greeting() {
+        return "Hello " ;
+    }
+
+}
