4ca995c4f0ab25b6c57af3fdd7cfee3110e27526
Lenovo <Lenovo@Lenovo-PC>
2017-08-20 12:50:53 +0530

    TEAM H- 2 services updated in Contract ManagementSellerService Class

--- src/main/java/com/rbs/training/supplychain/service/ContractManagementSellerService.java
+++ src/main/java/com/rbs/training/supplychain/service/ContractManagementSellerService.java
@@ -1,12 +1,57 @@
 package com.rbs.training.supplychain.service;
 
 import java.sql.Connection;
+
+
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
 
 import com.rbs.training.supplychain.DAO.DataBaseConnection;
+import com.rbs.training.supplychain.model.Proposal_Sellers_Bid;
 
-public class UpdateSellerResponseStatus {
+
+ @Service("ContractManagementSellerServiceObj")
+ 
+public class ContractManagementSellerService {
+	
+	/*service to fetch all the request for proposals(rfps)*/
+	
+	public List<Proposal_Sellers_Bid> listAllProposals(int seller_id){
+		
+		
+		DataBaseConnection dbobj = new DataBaseConnection();
+		List<Proposal_Sellers_Bid> lst = new ArrayList<Proposal_Sellers_Bid>();
+		Proposal_Sellers_Bid Proposal_Sellers_Bid_obj = null;
+		try{
+			Connection con = dbobj.getConnection();
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery("select * from proposal_sellers_bid where selle_rid='"+seller_id+"'");
+				Proposal_Sellers_Bid_obj = new Proposal_Sellers_Bid();
+				while(rs.next()){
+					Proposal_Sellers_Bid_obj.setProposal_id(rs.getInt("proposal_id"));
+					Proposal_Sellers_Bid_obj.setSeller_id(rs.getInt("seller_id"));
+					Proposal_Sellers_Bid_obj.setCost_avail(rs.getInt("cost_avail"));
+	
+					System.out.println(Proposal_Sellers_Bid_obj.toString());
+					lst.add(Proposal_Sellers_Bid_obj);
+			}
+			}
+			catch(Exception e)
+			{							
+				System.out.println(e.getMessage());
+			}
+		return lst;			
+	}
+	
+	
+/*sevice to update the seller's status(accept/reject/later) in the proposal_sellers_bid table 
+*/	
+	
 	public void updateSellerResponseStatus(int seller_id){
 	try{
 	DataBaseConnection dao = new DataBaseConnection();
@@ -16,7 +61,7 @@ public class UpdateSellerResponseStatus {
 
 	}
 	catch(Exception e){
-		System.out.println(e);
+		System.out.println(e.getMessage());
 	}
 	}
 }
