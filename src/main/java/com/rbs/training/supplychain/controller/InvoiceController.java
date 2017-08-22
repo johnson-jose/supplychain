22b8359117dc95613348563069663e0cb47cb346
murali1995 <murali.dharan1995@gmail.com>
2017-08-22 17:02:25 +0530

    murali

--- src/main/java/com/rbs/training/supplychain/controller/InvoiceController.java
+++ src/main/java/com/rbs/training/supplychain/controller/InvoiceController.java
@@ -1,22 +1,30 @@
 package com.rbs.training.supplychain.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.List;
 
 import javax.websocket.server.PathParam;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.rbs.training.supplychain.model.*;
-import com.rbs.training.supplychain.service.*;
+import com.rbs.training.supplychain.model.Invoice;
+import com.rbs.training.supplychain.service.InvoiceService;
 import com.rbs.training.supplychain.util.CustomMessage;
 
 @Component
@@ -74,5 +82,47 @@ public class InvoiceController {
 		}
 		return new ResponseEntity<CustomMessage>(msg,HttpStatus.OK);
     }
+	
+	
+	
+	
+	
+	private static String UPLOADED_FOLDER = "C://temp//";
 
+    @GetMapping("/")
+    public String index() {
+        return "/upload";
+    }
+
+    @PostMapping("/upload") // //new annotation since 4.3
+    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+
+        try {
+
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/uploadStatus";
+    }
+
+    @GetMapping("/invoice/uploadStatus")
+    public String uploadStatus() {
+        return "uploadStatus";
+    }
+	 
 }
