02189be71ef9887988e28610bf84d008d11eda31
johnson-jose <johnsonchoonattu@gmail.com>
2017-08-27 10:49:10 +0530

    Update InvoiceService.java
    invoiceitems imported

--- src/main/java/com/rbs/training/supplychain/service/InvoiceService.java
+++ src/main/java/com/rbs/training/supplychain/service/InvoiceService.java
@@ -30,6 +30,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.rbs.training.supplychain.DAO.DataBaseConnection;
 import com.rbs.training.supplychain.model.Invoice;
+import com.rbs.training.supplychain.model.InvoiceItems;
 import com.rbs.training.supplychain.util.CustomMessage;
 
 @Service("invoiceServiceObj")
