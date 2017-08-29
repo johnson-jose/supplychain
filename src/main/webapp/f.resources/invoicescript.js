46f4fb05fd352c0227b00a81d0fd3281610d204d
RBS <RBS@RBS1>
2017-08-29 16:28:51 +0530

    updated invoice update sankeerth

--- src/main/webapp/f.resources/invoicescript.js
+++ src/main/webapp/f.resources/invoicescript.js
@@ -27,6 +27,7 @@ var invoiceApp = angular.module('invoiceApp', ["ngRoute"]);
 	});
 
 	invoiceApp.controller('mainController', function($scope, $http){
+		
         $scope.deletefunc = function () {
         console.log("on delete function ");
 
@@ -86,25 +87,56 @@ var invoiceApp = angular.module('invoiceApp', ["ngRoute"]);
                     console.log($scope.productslist);
                 });
     	}
-        $scope.updatefunc = function () {
+        $scope.addItemFunc = function () {
+            
+        	console.log("in addItem function ");
+    			var invoiceNo = $scope.invoiceNo;
+    			console.log('invoice: ' + invoiceNo);
+    			console.log('Product id: ' + $scope.productID);
+    			console.log('quantity: ' + $scope.quantity);
+    			console.log('grossAmount: ' + $scope.grossAmount);
+    			console.log('tax: ' + $scope.tax);
+    			console.log('netAmount: ' + $scope.netAmount);
+    		
+    			/*  Add New product  */
+                $http.post('http://localhost:8181/invoice/addItems?invoiceID='+$scope.invoiceNo+'&productID='+$scope.productID+'&quantity='+ $scope.quantity+'&grossAmount='+$scope.grossAmount +'&tax='+$scope.tax +'&netAmount=' + $scope.netAmount)
+    			.success(function (data) {
+    				alert("Added new Item");
+    				$http.post('http://localhost:8181/invoice/viewProduct/?id=' + invoiceNo)
+        			.success(function (data) {
+                        $scope.productslist = data;
+                        console.log($scope.productslist);
+                    });
+                    $scope.productID="";
+                    $scope.quantity="";
+                    $scope.grossAmount="";
+                    $scope.tax="";
+                    $scope.netAmount="";
+                });
+                
+    	}
+        
+        $scope.deleteItemFunc = function () {
             
         	console.log("in update function ");
     			var invoiceNo = $scope.invoiceNo;
-    			console.log(invoiceNo);
-    			/*  view Invoice  */
+    			console.log('invoice: ' + invoiceNo);
+    			console.log('Product id: ' + $scope.delProductId);
+    			
+    			/*  view Invoice  
     			$http.post('http://localhost:8181/invoice/searchInvoice?invoiceID='+invoiceNo).success(function (data) {
     				console.log( "in updat after Search http");
     				$scope.invoice = data;
                     console.log($scope.invoice);
-                    window.location = "#/InvoiceUpdate";
-                });
-    			/*  view product  */
-                $http.post('http://localhost:8181/invoice/viewProduct/?id=' + invoiceNo)
+                });*/
+    			/*  delete product  */
+              $http.post('http://localhost:8181/invoice/deleteItem?invoiceID='+$scope.invoiceNo +'&productID=' + $scope.delProductId)
     			.success(function (data) {
                     $scope.productslist = data;
                     console.log($scope.productslist);
                 });
     	}
+        
 		$scope.landingfunc = function () {
 		console.log('go to landing page');
 		window.location = "#/InvoiceLanding";
